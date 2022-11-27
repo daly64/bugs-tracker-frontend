d54b41740d7a1d158b38ef97e6786f0918bd8204
Daly <dalyna64@gmail.com>
2022-11-27 15:07:25 +0100

    commit 27/11/2022

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Button, Col, Row} from "antd";
+import Search from "antd/es/input/Search.js";
+import {PlusOutlined} from "@ant-design/icons";
+
+
+const Style = {
+    root: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100vh',
+        width: '100vw',
+        // backgroundColor: 'orange',
+    },
+    navBar: {
+        // backgroundColor: 'lightblue',
+        height: '10%',
+        textAlign: 'center',
+        alignItems: 'center',
+        boxShadow: '12px 2px lightgray'
+
+    },
+}
+const {root, navBar} = Style
+
+function App() {
+    return (
+        <Col style={root}>
+            <Row style={navBar}>
+                <Col span={8}><h3>Bug Tracker</h3></Col>
+                <Col span={12}><Search placeholder="search project"/></Col>
+                <Col span={4}><Button icon={<PlusOutlined/>}> Add New Project </Button></Col>
+            </Row>
+        </Col>
+    )
+}
+
+export default App
+{/*<Title level={3}>Bug Tracker</Title>
+                <Search placeholder="search project"/>
+                <Button icon={<PlusOutlined/>}> Add New Project </Button>*/
+}
