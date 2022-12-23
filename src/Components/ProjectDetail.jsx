1bd407d7f0ce544b8d554956dd22480827ffed0d
Daly <dalyna64@gmail.com>
2022-12-23 23:19:11 +0100

    23/12/2022 start project update data function

--- src/Components/ProjectDetail.jsx
+++ src/Components/ProjectDetail.jsx
@@ -3,14 +3,15 @@ import {Button, Card, Col, Divider, List, Progress, Row, Typography} from "antd"
 import style from "../GeneralStyle.js";
 import {CheckOutlined, CloseOutlined, EditOutlined} from "@ant-design/icons";
 import {useNavigate} from "react-router-dom";
+import {updateProjectData} from "../Features/projectsFeatures.js";
 
 const {Paragraph, Title, Text} = Typography;
 
 const {detail, list, paragraph, mainColor, row} = style
 
 function ProjectDetail({project}) {
-    const navigate = useNavigate();
-
+    const navigate = useNavigate()
+    updateProjectData(project)
     return (
         <>
 
@@ -38,9 +39,11 @@ function ProjectDetail({project}) {
                                 /> </Title>}
                                   bordered>
                                 <List.Item> <Text>{project.bugs.length} total</Text> </List.Item>
-                                <List.Item><CheckOutlined style={{color: "green"}}/> <Text>{project.resolvedBugs} resolved</Text>
+                                <List.Item><CheckOutlined style={{color: "green"}}/>
+                                    <Text>{project.resolvedBugs} resolved</Text>
                                 </List.Item>
-                                <List.Item><CloseOutlined style={{color: "red"}}/> <Text>{project.unResolvedBugs} unresolved</Text>
+                                <List.Item><CloseOutlined style={{color: "red"}}/>
+                                    <Text>{project.unResolvedBugs} unresolved</Text>
                                 </List.Item>
                             </List>
                         </Col>
@@ -51,9 +54,11 @@ function ProjectDetail({project}) {
                                 />
                             </Title>} bordered>
                                 <List.Item> <Text>{project.features.length} total</Text> </List.Item>
-                                <List.Item><CheckOutlined style={{color: "green"}}/> <Text>{project.developedFeatures} developed</Text>
+                                <List.Item><CheckOutlined style={{color: "green"}}/>
+                                    <Text>{project.developedFeatures} developed</Text>
                                 </List.Item>
-                                <List.Item><CloseOutlined style={{color: "red"}}/> <Text>{project.unDevelopedFeatures} not developed</Text>
+                                <List.Item><CloseOutlined style={{color: "red"}}/>
+                                    <Text>{project.unDevelopedFeatures} not developed</Text>
                                 </List.Item>
                             </List>
                         </Col>
--- src/Features/projectsFeatures.js
+++ src/Features/projectsFeatures.js
@@ -2,8 +2,10 @@
 
 import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {addProject, findProject, getProjects, removeProject} from "./Store/projectsSlice.js";
+import {addProject, findProject, getProjects, removeProject, updateProject} from "./Store/projectsSlice.js";
 import store from "./Store/configureStore.js";
+import {useBugs} from "./bugsFeatures.js";
+import {useFeatures} from "./featuresFeatures.js";
 
 export function useProjects() {
     const dispatch = useDispatch()
@@ -44,5 +46,24 @@ export function deleteProject(project) {
 
 export function getProject(id) {
     return useProjects().find(project => project._id === id)
+}
+
+export function updateProjectData(project) {
+
+    let bugs = useBugs(project._id)
+    let features = useFeatures(project._id)
+    let resolvedBugs = 0
+    let unResolvedBugs = 0
+    let developedFeatures = 0
+    let unDevelopedFeatures = 0
+    let totalBugs = bugs.length
+    let totalFeatures = features.length
+    bugs.forEach(bug => bug.reserved ? resolvedBugs += 1 : unResolvedBugs += 1)
+    features.forEach(feature => feature.developed ? developedFeatures += 1 : unDevelopedFeatures += 1)
+    let progress = ((resolvedBugs + developedFeatures) / (totalBugs + totalFeatures))
+    console.log('progress =>', progress)
+    let updatedProject = {...project, progress, resolvedBugs, unResolvedBugs, developedFeatures, unDevelopedFeatures}
+    // store.dispatch(updateProject(updatedProject))
+
 }
 
